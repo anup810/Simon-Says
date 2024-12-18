976f6dd0206a076838d41e26c27c00c6fed96537
Anup Saud <anupsaud10@gmail.com>
2024-12-18 12:45:08 -0500

    feat: Add CircularButton class and initialize game UI
    Create CircularButton for circular buttons with highlight behavior. Set action button title with Futura-Bold font using attributed text. Sort buttons and labels by tag and initialize UI state in createNewGame().

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorButtons: [CircularButton]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var playersLabel: [UILabel]!
+    @IBOutlet var scoresLabel: [UILabel]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        colorButtons = colorButtons.sorted(by: {  $0.tag < $1.tag })
+        playersLabel = playersLabel.sorted(by: { $0.tag < $1.tag})
+        scoresLabel = scoresLabel.sorted(by: { $0.tag < $1.tag})
+        
+        createNewGame()
+   
+    }
+    func createNewGame() {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Futura-Bold", size: 25) ?? UIFont.boldSystemFont(ofSize: 25)
+        ]
+
+        let attributedTitle = NSAttributedString(string: "Start Game", attributes: attributes)
+        actionButton.setAttributedTitle(attributedTitle, for: .normal)
+
+
+        for button in colorButtons {
+            button.alpha = 0.5
+        }
     }
 
-
+    
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        print("Button \(sender.tag) tapped")
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+    }
+    
 }
 
